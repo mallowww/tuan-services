618ca9dfc6d30079601cfa4feffd78d4cb609e33
mell <chanon4817@gmail.com>
2023-04-18 19:23:31 +0700

    feat: mariadb, products adapter

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/labstack/echo/v4"
-)
+import "gorm.io/gorm"
 
 func main() {
-	e := echo.New()
-	e.GET("/home", func(c echo.Context) error {
-		time.Sleep(time.Millisecond * 10)
-		return c.String(http.StatusOK, "ok")
-	})
-	err := e.Start(":8088")
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-		os.Exit(1)
-	}
+
+}
+
+func initDB() *gorm.DB {
 
 }
---
+++ repositories/product_db.go
@@ -0,0 +1,45 @@
+package repositories
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type productRepositoryDB struct {
+	db *gorm.DB
+}
+
+func NewProductRepositoryDB(db *gorm.DB) ProductRepository {
+	db.AutoMigrate(&product{})
+	mockData(db)
+	return productRepositoryDB{db: db}
+}
+
+func mockData(db *gorm.DB) error {
+
+	var count int64
+	db.Model(&product{}).Count(&count)
+	if count > 0 {
+		return nil
+	}
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	products := []product{}
+	for i := 0; i < 5000; i++ {
+		products = append(products, product{
+			Name:     fmt.Sprintf("Product %v", i+1),
+			Quantity: random.Intn(100),
+		})
+	}
+	return db.Create(&products).Error
+}
+
+func (r productRepositoryDB) GetProduct() (products []product,err error) {
+	err = r.db.Order("Quantity descending").Limit(20).Find(&products).Error
+	return products, err
+}
