56bd6cb884dfe7319524be8f2572c5ece0764852
mell <chanon4817@gmail.com>
2023-04-21 04:57:25 +0700

    feat: connect to mariadb, add service adminer

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/mallowww/tuan-redis/repositories"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-
+	db := initDB()
+	productRepo := repositories.NewProductRepositoryDB(db)
+	products, err := productRepo.GetProduct()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(products)
 }
 
 func initDB() *gorm.DB {
-
+	// dial := mysql.Open("root:notApengu1ndb@tcp(localhost:3350)/penguin")
+	// db, err := gorm.Open(dial, &gorm.Config{})
+	dsn := "root:notApengu1ndb@tcp(localhost:3306)/penguin?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
