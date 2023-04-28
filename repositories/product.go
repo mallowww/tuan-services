664ce40ed36ecd5e61d03957789123f9e3b725bd
mell <chanon4817@gmail.com>
2023-04-28 09:44:10 +0700

    feat: product redis database

--- repositories/product.go
+++ repositories/product.go
@@ -1,5 +1,13 @@
 package repositories
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type product struct {
 	ID       int
 	Name     string
@@ -8,4 +16,25 @@ type product struct {
 
 type ProductRepository interface {
 	GetProduct() ([]product, error)
+}
+
+func mockData(db *gorm.DB) error {
+
+	var count int64
+	db.Model(&product{}).Count(&count)
+	if count > 0 {
+		return nil
+	}
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	products := []product{}
+	for i := 0; i < 5000; i++ {
+		products = append(products, product{
+			Name:     fmt.Sprintf("Product %v", i+1),
+			Quantity: random.Intn(100),
+		})
+	}
+	return db.Create(&products).Error
 }
--- repositories/product_db.go
+++ repositories/product_db.go
@@ -1,10 +1,6 @@
 package repositories
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
-
 	"gorm.io/gorm"
 )
 
@@ -18,28 +14,7 @@ func NewProductRepositoryDB(db *gorm.DB) ProductRepository {
 	return productRepositoryDB{db: db}
 }
 
-func mockData(db *gorm.DB) error {
-
-	var count int64
-	db.Model(&product{}).Count(&count)
-	if count > 0 {
-		return nil
-	}
-
-	seed := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(seed)
-
-	products := []product{}
-	for i := 0; i < 5000; i++ {
-		products = append(products, product{
-			Name:     fmt.Sprintf("Product %v", i+1),
-			Quantity: random.Intn(100),
-		})
-	}
-	return db.Create(&products).Error
-}
-
-func (r productRepositoryDB) GetProduct() (products []product,err error) {
+func (r productRepositoryDB) GetProduct() (products []product, err error) {
 	err = r.db.Order("Quantity DESC").Limit(20).Find(&products).Error
 	return products, err
 }
